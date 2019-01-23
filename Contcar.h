a31abcc34da032e4a7a805d786a6cfdbb4e5154f
cbarger233 <cbarger233@gmail.com>
2019-01-22 20:36:35 -0500

    Update Contcar.h

--- Contcar.h
+++ Contcar.h
@@ -394,14 +394,14 @@ void Contcar::get_coordination() {
 	//checking all of the carbon atoms for coordination defects
 	for (unsigned i = 0; i < carbons.size(); i++) {
 		for (unsigned j = 0; j < silicons.size(); j++) {
+			distance = find_distance(silicons[j], carbons[i]);
+			if (distance < SiC_bond_length && distance > 0.1)
+				bonds++;
 			for (unsigned k = 0; k < hydrogens.size(); k++) {
-				for (unsigned z = 0; z < carbons.size(); z++) {
-					distance = find_distance(silicons[j], carbons[i]);
-					if (distance < SiC_bond_length && distance > 0.1)
-						bonds++;
-					distance = find_distance(carbons[i], hydrogens[k]);
+				distance = find_distance(carbons[i], hydrogens[k]);
 					if (distance < CH_bond_length && distance > 0.1)
 						bonds++;
+				for (unsigned z = 0; z < carbons.size(); z++) {
 					if (i != z)
 						distance = find_distance(carbons[i], carbons[z]);
 					if (distance < CC_bond_length && distance > 0.1)
@@ -419,14 +419,14 @@ void Contcar::get_coordination() {
 	//checking all of the silicon atoms for coordination defects
 	for (unsigned i = 0; i < silicons.size(); i++) {
 		for (unsigned j = 0; j < carbons.size(); j++) {
-			for (unsigned k = 0; k < hydrogens.size(); k++) {
-				for (unsigned z = 0; z < silicons.size(); z++) {
-					distance = find_distance(silicons[i], carbons[j]);
+			distance = find_distance(silicons[i], carbons[j]);
 					if (distance < SiC_bond_length && distance > 0.1)
 						bonds++;
-					distance = find_distance(silicons[i], hydrogens[k]);
+			for (unsigned k = 0; k < hydrogens.size(); k++) {
+				distance = find_distance(silicons[i], hydrogens[k]);
 					if (distance < SiH_bond_length && distance > 0.1)
 						bonds++;
+				for (unsigned z = 0; z < silicons.size(); z++) {
 					if (i != z)
 						distance = find_distance(silicons[i], silicons[z]);
 					if (distance < SiSi_bond_length && distance > 0.1)
